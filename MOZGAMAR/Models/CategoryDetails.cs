c3b1027433a5a795532210e3847e41f26e85b328
fausio luis <fausioluis@live.com>
2020-05-10 10:14:43 +0200

    adding category model

---
+++ MOZGAMAR/Models/CategoryDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MOZGAMAR.Models
+{
+    public class CategoryDetails
+    {
+        public int CategoryId { get; set; }
+        [Required(ErrorMessage ="Nome da categoria é obrigatório")]
+        [StringLength(100,ErrorMessage = "O nome da categoria deve ter no maximo 100 caracteres e no minimo 3",MinimumLength =3)]
+        public string Name { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> ISDelete { get; set; }
+    }
+
+
+}
