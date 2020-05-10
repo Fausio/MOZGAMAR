79d0bf0dc3d35d566692f6410f47a1f2979d0c63
fausio luis <fausioluis@live.com>
2020-05-10 10:25:11 +0200

    adding shipping.cs model

---
+++ MOZGAMAR/Models/Shipping.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MOZGAMAR.Models
+{
+    public class Shipping
+    {
+        public int ShippingDetaisId { get; set; }
+        [Required]
+        public Nullable<int> MemberId { get; set; }
+        [Required]
+        public string Addres { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Sate { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string ZipCode { get; set; }
+        public Nullable<int> OrderId { get; set; }
+        public Nullable<decimal> AmoutPaId { get; set; }
+        [Required]
+        public string PaymentType { get; set; }
+    }
+}
