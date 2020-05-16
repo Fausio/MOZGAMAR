ea6004a8d3695e15b7725cd4414ed43d5f353362
fausio luis <fausioluis@live.com>
2020-05-16 18:16:07 +0200

    Adding delete or Update action

--- MOZGAMAR/Controllers/AdminController.cs
+++ MOZGAMAR/Controllers/AdminController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using MOZGAMAR.DAL;
+using MOZGAMAR.Models;
+using MOZGAMAR.Repository;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +13,41 @@ namespace MOZGAMAR.Controllers
 {
     public class AdminController : Controller
     {
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
         // GET: Admin
         public ActionResult Dashboard()
         {
             return View();
         }
-    }
+
+        public ActionResult Category()
+        {
+            List<Category> allcategories = _unitOfWork.GetRepositoryInstance<Category>().GetAllRecordsIQueryable().Where(x => x.ISDelete == false).ToList();
+            return View(allcategories);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return UpdateCategory(0);
+        }
+
+
+        public ActionResult UpdateCategory(int Id)
+        {
+            CategoryDetails cd;
+            if (Id != 0 & Id > 0)
+            {
+                cd = JsonConvert.DeserializeObject<CategoryDetails>(JsonConvert.SerializeObject(_unitOfWork.GetRepositoryInstance<Category>().GetFirstOrDefault(Id)));
+            }
+            else
+            {
+                cd = new CategoryDetails();
+            }
+
+            return View("UpdateCategory", cd);
+
+        }
+
+}
 }
