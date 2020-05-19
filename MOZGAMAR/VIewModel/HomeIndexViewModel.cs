66cb4f86c6a00c3bfe0103ff7c08890615aad702
fausio luis <fausioluis@live.com>
2020-05-19 18:53:05 +0200

    page filtering

--- MOZGAMAR/VIewModel/HomeIndexViewModel.cs
+++ MOZGAMAR/VIewModel/HomeIndexViewModel.cs
@@ -1,23 +1,31 @@
 ﻿using MOZGAMAR.DAL;
 using MOZGAMAR.Repository;
+using PagedList;
+using PagedList.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MOZGAMAR.VIewModel
 {
     public class HomeIndexViewModel
     {
         public GenericUnitOfWork _UnitOfWork = new GenericUnitOfWork();
+        MOZGAMEREntities context = new MOZGAMEREntities();
 
-        public IEnumerable<Poduct> LiftOfproducts = new List<Poduct>();
+        public IPagedList<Poduct> LiftOfproducts { get; set; }
 
-        public HomeIndexViewModel CreateModel()
+        public HomeIndexViewModel CreateModel(string pesquisa, int pageSize, int? page)
         {
+            SqlParameter[] sql = new SqlParameter[] { new SqlParameter("@search", pesquisa ?? (object)DBNull.Value) };
+            IPagedList<Poduct> DataFilter = context.Database.SqlQuery<Poduct>("GEtBySerach @search", sql).ToList().ToPagedList(page ?? 1, pageSize);
+
             return new HomeIndexViewModel()
             {
-                LiftOfproducts = _UnitOfWork.GetRepositoryInstance<Poduct>().GetAllRecords()
+                LiftOfproducts = DataFilter
             };
         }
     }
