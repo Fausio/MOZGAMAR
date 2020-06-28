3b6ed9db911699bcf05f58849cd99948f4183639
fausio luis <fausioluis@live.com>
2020-06-28 11:00:05 +0200

    Adding services  to the project and remove trigger implementation

---
+++ EFDBAcess/Service/ProductService.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using EFDBAcess.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDBAcess.Service
+{
+    public class ProductService
+    { 
+        private GenericUnitOfWork genericUnitOfWork = new GenericUnitOfWork();
+       
+        public List<Product> GetProductAndCaregoryByName(string search)
+        {
+            if (search == null)
+                return genericUnitOfWork.DBEntity.Product.ToList();
+            else
+            {
+                return genericUnitOfWork.DBEntity.Database.SqlQuery<Product>(
+                                                                            @"SELECT  p.*
+                                                                             FROM	Products AS p
+                                                                             LEFT JOIN Categories AS C ON  C.CategoryId = p.CategoryId 
+                                                                             WHERE p.Name like '%'+@search+'%'  OR c.Name like '%'+@search+'%'
+                                                                             ",
+              new SqlParameter("search", search )).ToList();
+            }
+        } 
+    }
+}
