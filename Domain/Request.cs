cf9e3f1198d25594ac24f09d968f2c59db2b7d9a
fausio luis <fausioluis@live.com>
2020-07-12 14:18:32 +0200

    RequestsController

--- Domain/Request.cs
+++ Domain/Request.cs
@@ -17,6 +17,7 @@ namespace Domain
         public string Description { get; set; }
 
         [Required]
+        [Display(Name = "Categoria")]
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
 
---
+++ MOZGAMAR/Controllers/RequestsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Domain;
+using EFDBAcess;
+
+namespace MOZGAMAR.Controllers
+{
+    public class RequestsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Requests
+        public ActionResult Index()
+        {
+            var requests = db.Requests.Include(r => r.Category);
+            return View(requests.ToList());
+        }
+
+        // GET: Requests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.Requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // GET: Requests/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Category, "CategoryId", "Name");
+            return View();
+        }
+
+        // POST: Requests/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "requestId,Name,Brand,InitialPrice,FinalPrice,Description,CategoryId")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Requests.Add(request);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Category, "CategoryId", "Name", request.CategoryId);
+            return View(request);
+        }
+
+        // GET: Requests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.Requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "CategoryId", "Name", request.CategoryId);
+            return View(request);
+        }
+
+        // POST: Requests/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "requestId,Name,Brand,InitialPrice,FinalPrice,Description,CategoryId")] Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(request).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Category, "CategoryId", "Name", request.CategoryId);
+            return View(request);
+        }
+
+        // GET: Requests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Request request = db.Requests.Find(id);
+            if (request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(request);
+        }
+
+        // POST: Requests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Request request = db.Requests.Find(id);
+            db.Requests.Remove(request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
