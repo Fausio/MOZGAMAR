c1b020621f7aa96f583dbc9a6283c5b6041e4a80
fausio luis <fausioluis@live.com>
2020-07-12 14:13:48 +0200

    Request

---
+++ Domain/Request.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Request
+    {
+        public int requestId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public decimal InitialPrice { get; set; }
+        public decimal FinalPrice { get; set; }
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+    }
+}
